bc55a2328552582511d703ca7f6decb9298b21c4
Guty Santana <agustin.santana@gmail.com>
2021-07-01 18:14:22 -0500

    Uso de dialogos de alerta y avatar

--- lib/src/pages/alert.dart
+++ lib/src/pages/alert.dart
@@ -8,7 +8,50 @@ class AlertPage extends StatelessWidget {
       appBar: AppBar(
         title: const Text('Alertas'),
       ),
+      body: Center(
+        child: ElevatedButton(
+          onPressed: () => _displayAlert(context),
+          style: ElevatedButton.styleFrom(
+            primary: Colors.blue.shade600,
+            shape: const StadiumBorder(),
+          ),
+          child: const Text('Activar Alerta'),
+        ),
+      ),
       floatingActionButton: getBackButton(context),
+    );
+  }
+
+  void _displayAlert(BuildContext context) {
+    showDialog(
+      context: context,
+      barrierDismissible: true,
+      builder: (context) {
+        return AlertDialog(
+          backgroundColor: Colors.lightBlue.shade50,
+          elevation: 200.0,
+          shape:
+              RoundedRectangleBorder(borderRadius: BorderRadius.circular(30.0)),
+          title: const Text('Diálogo'),
+          content: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: const [
+              Text('Este es el contenido de mi alerta'),
+              FlutterLogo(size: 100.0),
+            ],
+          ),
+          actions: [
+            TextButton(
+              onPressed: () => Navigator.of(context).pop(),
+              child: const Text('Cancelar'),
+            ),
+            TextButton(
+              onPressed: () => Navigator.of(context).pop(),
+              child: const Text('Ok'),
+            ),
+          ],
+        );
+      },
     );
   }
 }
--- lib/src/pages/avatar_page.dart
+++ lib/src/pages/avatar_page.dart
@@ -7,6 +7,27 @@ class AvatarPage extends StatelessWidget {
     return Scaffold(
       appBar: AppBar(
         title: const Text('Avatars'),
+        actions: [
+          Container(
+            margin: const EdgeInsets.only(right: 5.0),
+            child: const CircleAvatar(
+              backgroundImage: AssetImage('assets/girl-avatar.jpg'),
+            ),
+          ),
+          Container(
+            margin: const EdgeInsets.only(right: 10.0),
+            child: CircleAvatar(
+              backgroundColor: Colors.blue.shade800,
+              child: const Text('AS'),
+            ),
+          )
+        ],
+      ),
+      body: Center(
+        child: FadeInImage(
+            placeholder: AssetImage('assets/Infinity-loading.gif'),
+            image: NetworkImage(
+                'http://30f3l25np0o2lx1uq28eg3or-wpengine.netdna-ssl.com/wp-content/uploads/2013/02/North_Shore_Holiday_By_Heather_Brown.jpg')),
       ),
       floatingActionButton: getBackButton(context),
     );
